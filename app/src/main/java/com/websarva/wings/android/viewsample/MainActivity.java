f24297f95d1c3eeb73a2324f393dfff5631fdb10
柳原君子 <yanagiharakimiko@yanagiharakimikonoMacBook-Pro.local>
2021-07-17 19:05:37 +0900

    first commit

---
+++ app/src/main/java/com/websarva/wings/android/viewsample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.websarva.wings.android.viewsample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
